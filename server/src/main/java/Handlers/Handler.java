6e83afc6ec9b740a4f08acb594228b02eee68830
Andy Mam <118705791+andymam@users.noreply.github.com>
2024-02-20 13:37:40 -0700

    a lil more setup

---
+++ server/src/main/java/Handlers/Handler.java
@@ -0,0 +1,35 @@
+package Handlers;
+
+public class Handler {
+
+
+
+}
+
+class clearHandler extends Handler {
+
+}
+
+class registerHandler extends Handler {
+
+}
+
+class loginHandler extends Handler {
+
+}
+
+class logoutHandler extends Handler {
+
+}
+
+class listGamesHandler extends Handler {
+
+}
+
+class createGameHandler extends Handler {
+
+}
+
+class joinGameHandler extends Handler {
+
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,22 +1,44 @@
 package server;
 
 import spark.*;
+import java.util.Map;
+import service.*;
 
 public class Server {
 
-    public int run(int desiredPort) {
+    public static void main(String[] args) {
+        new Server().run(8080);
+    }
+
+    public Server run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", this::clearDB);
+        Spark.post("/user", this::registerUser);
+        Spark.post("/session", this::loginUser);
+        Spark.delete("/session", this::logoutUser);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
+
 
         Spark.awaitInitialization();
+        return this;
+    }
+
+    public int port() {
         return Spark.port();
     }
 
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object clearDB(Request req, Response res) throws ResponseException {
+
     }
 }
